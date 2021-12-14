c12161c56789c191e589a83288d043b9da339022
vivekjoy <vivek.joy@litmus7.com>
2021-12-14 13:46:22 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+function App() {
+  return (
+    <div>
+      <h1>Hi There</h1>
+      <MarketingApp />
+    </div>
+  )
+}
+
+export default App
