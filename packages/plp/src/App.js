84927f40201a48a53211b346bce16bb54982a3e1
vivekjoy <vivek.joy@litmus7.com>
2021-12-21 14:27:52 +0530

    removed unused import

--- packages/plp/src/App.js
+++ packages/plp/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
-import Stats from './components/Stats/Stats'
-import Container from '@material-ui/core/Container'
-import ProductList from './components/ProductList'
-import Filter from './components/Filter'
+
 import PlpContainer from './components/PlpContainer'
 
 const generateClassName = createGenerateClassName({
